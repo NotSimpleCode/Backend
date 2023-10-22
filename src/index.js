17c68374bfd705eba2316e1be0eac66f4d05f8ac
NotSimpleCode <onejobsdiego@gmail.com>
2023-10-21 22:16:53 -0500

    bug fix 404

--- src/index.js
+++ src/index.js
@@ -3,7 +3,14 @@ import cors from "cors"; // Importa el paquete cors
 import peopleRoutes from "./routes/people_routes.js";
 import rolesRoutes from "./routes/roles_routes.js";
 import userConnection from "./routes/users_connection_routes.js";
-
+import cosechas from "./routes/cosechas_routes.js";
+import hist_cosecha from "./routes/historial_cosecha_routes.js";
+import hist_plagas from "./routes/historial_plagas_routes.js";
+import lotes from "./routes/lotes_routes.js";
+import user_lotes from "./routes/lotes_users_routes.js";
+import sectores from "./routes/sectores_routes.js";
+import tipo_planta from "./routes/tipo_plantas_routes.js";
+import ventas from "./routes/ventas_routes.js";
 
 const app = express();
 
@@ -17,6 +24,15 @@ app.use(cors());
 app.use('/api', peopleRoutes); 
 app.use('/api', rolesRoutes);
 app.use('/api', userConnection);
+
+app.use('/api', lotes); 
+app.use('/api', user_lotes);
+app.use('/api', tipo_planta);
+app.use('/api', sectores); 
+app.use('/api', cosechas);
+app.use('/api', hist_cosecha);
+app.use('/api', hist_plagas); 
+app.use('/api', ventas);
 
 
 const port = process.env.PORT || 3000; // Usa el puerto proporcionado por Azure o el puerto 3000 si no está definido
